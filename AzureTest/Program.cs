bb959f0a0c52ec2f01d0e81aa95f85827202c513
pcmichaels <pcmichaels@live.com>
2017-05-26 07:45:03 +0100

    Now adds a new message to the queue

--- AzureTest/Program.cs
+++ AzureTest/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.ServiceBus.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +19,8 @@ namespace AzureTest
 
             CreateNewQueue(uri, tokenProvider);
 
+            AddNewMessage("1", "test", "TestQueue");
+
             Console.WriteLine("Done");
             Console.ReadLine();
         }
@@ -30,7 +34,7 @@ namespace AzureTest
             return tp;
         }
 
-        private static void CreateNewQueue(Uri uri, TokenProvider tokenProvider)
+        private static NamespaceManager CreateNewQueue(Uri uri, TokenProvider tokenProvider)
         {
             Console.WriteLine($"Creating new queue...");
             NamespaceManager nm = new NamespaceManager(uri, tokenProvider);
@@ -45,6 +49,47 @@ namespace AzureTest
                 Console.WriteLine("Creating new queue");
                 QueueDescription qd = nm.CreateQueue("TestQueue");
             }
+
+            return nm;
+        }
+
+        private static void AddNewMessage(string id, string messageBody, string queueName)
+        {
+            BrokeredMessage message = new BrokeredMessage(messageBody)
+            {
+                MessageId = id
+            };
+
+            string connectionString = GetConnectionString();
+            
+            QueueClient queueClient = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            queueClient.Send(message);
+        }
+        
+        private static string GetConnectionString()
+        {
+            return "Endpoint=sb://pcm-servicebustest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=JWh82nkstIAi4w5tW6MEj7GKQfoiZlwBYjHx9wfDqdA=";                    
+            
+            /*
+            string Url = string.Format("https://management.core.windows.net/{0}/services/servicebus/namespaces/{1}/ConnectionDetails", subscriptionId, namespaceId);
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(Url);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(Url).Result;  // Blocking call!
+            if (response.IsSuccessStatusCode)
+            {                
+                string result = response.Content.ReadAsStringAsync().Result;
+
+                return result;
+            }
+
+            throw new Exception("Could not get connection string");
+            */
         }
 
         private static Uri GetServiceUri()
